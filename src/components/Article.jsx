b920f2d992ef15b18057228c16f1f3c26b979c84
TypeVee <veronicaiswriting@gmail.com>
2023-10-26 15:34:07 +0100

    Added loading bar

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,22 +1,31 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { fetchArticle } from "../api"
+import LoadingIcon from "./LoadingIcon.jsx"
 
 export default function Article(){
     const {article_id} = useParams()
     const [article, setArticle] = useState({})
-
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(()=>{
         fetchArticle(article_id).then(({data})=>{
+            console.log("Loading")
+            setIsLoading(false)
             setArticle(data.article)
         })    
     }, [])
+    const Article = (
+        <div>
+        <h1>{article.title}</h1>
+        <img src={article.article_img_url}/>
+        <p>{article.body}</p>
+        </div>
+    )
+
 
     return(
         <div>
-            <h1>{article.title}</h1>
-            <img src={article.article_img_url}/>
-            <p>{article.body}</p>
+        {isLoading ? LoadingIcon() : Article}
         </div>
     )
 }
